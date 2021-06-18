08c16f1e79e7d105ee367bf67a5987e5c0be6734
faizanpro77 <faizyshaikh204@gmail.com>
2021-06-18 12:41:05 +0530

    ADDED: add functionality to check number of record matches

--- src/test/java/com/bridgelabz/CensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/CensusAnalyserTest.java
@@ -5,11 +5,15 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 public class CensusAnalyserTest {
+
     private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
     private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
     private static final String WRONG_CSV_TYPE = "./src/test/resources/IndiaStateCensusData.txt";
     private static final String SAMPLE_CSV_DELIMITERS = "./src/test/resources/DelimiterFile.csv";
     private static final String SAMPLE_CSV_HEADER = "./src/test/resources/HeaderFile.csv";
+
+    private static final String STATE_CODE_CSV_FILE_PATH = "./src/test/resources/IndiaStateCode.csv";
+
 
 
     @Test
@@ -62,4 +66,13 @@ public class CensusAnalyserTest {
             Assert.assertEquals(CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE, e.type);
         }
     }
+
+    //UC_2
+    @Test
+    public void given_IndianStateCodeCSVFile_ReturnsCorrectRecords() throws CensusAnalyserException {
+        CensusAnalyser censusAnalyser = new CensusAnalyser();
+        int numOfRecords = censusAnalyser.loadIndiaStateCode(STATE_CODE_CSV_FILE_PATH);
+        Assert.assertEquals(37, numOfRecords);
+    }
+
 }
