d4f01d66d3f8f6aa27881f367a4fea14e7a147cb
faizanpro77 <faizyshaikh204@gmail.com>
2021-06-18 10:42:51 +0530

    ADDED: handle exception for incorrect delimiter

--- src/main/java/com/bridgelabz/CensusAnalyserException.java
+++ src/main/java/com/bridgelabz/CensusAnalyserException.java
@@ -12,4 +12,9 @@ class CensusAnalyserException extends Exception {
         super(message);
         this.type = type;
     }
+
+    public CensusAnalyserException(String message, ExceptionType type, Throwable cause) {
+        super(message, cause);
+        this.type = type;
+    }
 }
--- src/test/java/com/bridgelabz/CensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/CensusAnalyserTest.java
@@ -8,6 +8,7 @@ public class CensusAnalyserTest {
     private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
     private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
     private static final String WRONG_CSV_TYPE = "./src/test/resources/IndiaStateCensusData.txt";
+    private static final String SAMPLE_CSV_DELIMITERS = "./src/test/resources/DelimiterFile.csv";
 
 
     @Test
@@ -38,6 +39,16 @@ public class CensusAnalyserTest {
             censusAnalyser.loadIndiaCensusData(WRONG_CSV_TYPE);
         }catch (CensusAnalyserException e){
             Assert.assertEquals(CensusAnalyserException.ExceptionType.NOT_A_CSV_TYPE,e.type);
+        }
+    }
+
+    @Test
+    public void givenIndiaCensusData_WhenDelimiterIncorrect_ShouldThrowException() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            censusAnalyser.loadIndiaCensusData(SAMPLE_CSV_DELIMITERS);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE, e.type);
         }
     }
 }
